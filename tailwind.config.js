7da91cbcc7a694a2e6df02838b4aab3723fb6e0d
Scott Rasmussen (Zaita) <scott@zaita.com>
2023-12-12 20:25:15 +1300

    ADD: More initial git files

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', 'Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+              basedarkdarkred: "#700000",
+              basedarkred : "#800000",
+              basered : "#AB0000",
+              themeMain : "var(--theme-main-colour)",
+            }
+        },
+    },
+
+    plugins: [forms],
+};
