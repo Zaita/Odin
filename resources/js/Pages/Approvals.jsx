546a6ecaaac594044b98fc8f6ecf122c298d4e12
Scott Rasmussen (Zaita) <scott@zaita.com>
2024-01-15 22:58:09 +1300

    ADD: Basic /approvals page functionality

--- resources/js/Pages/Approvals.jsx
+++ resources/js/Pages/Approvals.jsx
@@ -1,10 +1,33 @@
+import { Link } from '@inertiajs/react';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
 import UserLayout from '@/Layouts/UserLayout';
 
 export default function Approvals(props) {
   function Content(props) {
     return (
       <div id="content_box" className="mt-5">
-        Approvals
+        <div className="float-right" style={{fontSize: "11px"}}>Displaying entries</div>
+          <div className="w-full flex bg-white mb-1 p-2">
+              <div className="w-2/12 font-bold">Date Created</div>
+              <div className="w-2/12 font-bold">Pillar</div>
+              <div className="w-3/12 font-bold">Product Name</div>
+              <div className="w-2/12 font-bold">Tasks Completed</div>
+              <div className="w-2/12 font-bold">Submitter</div>
+              <div className="w-1/12 font-bold">Actions</div>
+          </div>
+          {props.submissions.map((submission, index) => (
+            <div className="w-full flex bg-white mb-0 p-1">
+              <div className="w-2/12">{submission.created_at_short}</div>
+              <div className="w-2/12">{submission.pillar_name}</div>
+              <div className="w-3/12">{submission.product_name}</div>
+              <div className="w-2/12">{submission.tasks_completed}</div>
+              <div className="w-2/12">{submission.submitter_name}</div>
+              <div className="w-1/12">
+                  <Link href={"/view/" + submission.uuid} style={{color: props.siteConfig.themeHyperlinkColor}}><ChevronRightIcon/></Link>
+              </div>
+            </div>
+          ))}
       </div>
     )
   }
