272406f72cb5d4653d65ae2c4435ef3eb4c7389c
Scott Rasmussen (Zaita) <scott@zaita.com>
2024-02-20 23:06:00 +1300

    ADD: Framework for custom risks on questionnaires

---
+++ app/Models/QuestionnaireRisk.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionnaireRisk extends Model
+{
+    use HasFactory;
+}
