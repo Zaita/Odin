5fab4fffeaa169f6903ed81f70a9679475350ea4
Scott Rasmussen (Zaita) <scott@zaita.com>
2024-05-13 22:47:23 +1200

    CHG: Removing stuff from CSS because it someones breaks formatting

--- resources/js/Pages/Home/LatestSubmissions.jsx
+++ resources/js/Pages/Home/LatestSubmissions.jsx
@@ -3,10 +3,10 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 export default function LatestSubmissions(props) {
   return (
-    <div id="latest_submissions" className="mb-4">
-      <div id="heading">Your latest submissions</div>
-      <div id="submission_box">
-      <div className="w-full flex p-2 mb-1" style={{backgroundColor: props.siteConfig.theme_content_bg_color}}>
+    <div className="mb-4 w-3/5 ml-auto mr-auto mt-5">
+      <div className="text-sm font-bold mt-4 mb-4">Your latest submissions</div>
+      <div>
+      <div className="w-full flex p-2 mb-0.5" style={{backgroundColor: props.siteConfig.theme_content_bg_color}}>
         <div className="w-2/12 font-bold">Date Created</div>
         <div className="w-2/12 font-bold">Pillar</div>
         <div className="w-3/12 font-bold">Product Name</div>
@@ -15,7 +15,7 @@ export default function LatestSubmissions(props) {
         <div className="w-1/12 font-bold">Actions</div>
       </div>
       {props.latestSubmissions.map((submission, index) => (
-        <div className="w-full flex mb-1 p-1" style={{backgroundColor: props.siteConfig.theme_content_bg_color}} key={index}>
+        <div className="w-full flex mb-0.5 p-1" style={{backgroundColor: props.siteConfig.theme_content_bg_color}} key={index}>
           <div className="w-2/12">{submission.created_at_short}</div>
           <div className="w-2/12">{submission.pillar_name}</div>
           <div className="w-3/12">{submission.product_name}</div>
